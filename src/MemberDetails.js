1973526da247cf2793ec33e2361a1a44d349333f
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-06-21 16:23:13 -0400

    2nd commit

---
+++ src/MemberDetails.js
@@ -0,0 +1,10 @@
+// import { useEffect } from "react";
+
+// function MemberDetails(){
+//     const [memberDetails, setMemberDetails]=useState([]);
+//     const {id} = 1
+
+//     useEffect(() => {
+//         fetch
+//     })
+// }
