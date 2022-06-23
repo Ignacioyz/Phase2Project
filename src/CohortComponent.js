7561ed21b6688219793ab8d8f9fadb7a6d46a6c9
Ignacio Yz <ignacioyz@gmail.com>
2022-06-23 13:46:13 -0400

    not much

--- src/CohortComponent.js
+++ src/CohortComponent.js
@@ -5,31 +5,52 @@ import StudentProfile from "./StudentProfile"
 
 
 const CohortComponent = ({ cohort, handleClick, memberProfile, }) => {
-     
-    
+
+
+    function postNewMember(e) {
+        fetch('http://localhost:4000/students', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application.json',
+                'Content-Type': 'application.json'
+            },
+            body: JSON.stringify(
+                ({
+                    'Image': e.target.Image.value,
+                    'Name':e.target.Name.value,
+                    'City':e.target.City.value,
+                    'FavoriteFood':e.target.FavoriteFood.value,
+                    'FavoriteColor':e.target.FavoriteColor.value,
+
+                })
+            )
+        })
+
+    }
+
     return (
         <div>
-            {cohort.map(member => {     
+            {cohort.map(member => {
                 return (
-                    <div key={member.Name} onClick={() => handleClick(member) } id="CohortMembers" > 
-                   
-                    <img src={member.Image}></img>
-                    <br></br>
-                    
-                     <br></br>
-                     {/* Place of birth: {member.City} 
+                    <div key={member.Name} onClick={() => handleClick(member)} id="CohortMembers" >
+
+                        <img src={member.Image}></img>
+                        <br></br>
+
+                        <br></br>
+                        {/* Place of birth: {member.City} 
                      <br></br> 
                      Favorite Food: {member.FavoriteFood} 
                      <br></br>  
                      Favorite Color: 
                      <br></br> 
                      {member.FavoriteColor}   */}
-                     
-                     
-                    <NavLink to={`/StudentProfile/${member.id}`}> {member.Name} </NavLink>
-                     </div>
-                
-                
+
+
+                        <NavLink to={`/StudentProfile/${member.id}`}> {member.Name} </NavLink>
+                    </div>
+
+
                 )
 
 
